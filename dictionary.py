2b29ef7f87284e39b58a4240875ef7137df91d73
papercutss <kuznetsov.anton00@gmail.com>
2025-05-25 22:33:47 +0300

    Changes

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-definitions = {
+efinitions = {
     "python": "Язык программирования общего назначения.",
     "git": "Система контроля версий.",
     "алгоритм": "Последовательность действий для решения задачи.",
@@ -11,3 +11,8 @@ if word in definitions:
     print(f"{word.capitalize()}: {definitions[word]}")
 else:
     print("Слово не найдено в словаре.")
+    choice = input("Хотите добавить определение для этого слова? (да/нет): ").lower()
+    if choice == "да":
+        definition = input("Введите определение: ")
+        definitions[word] = definition
+        print(f"Слово '{word}' добавлено в словарь.")
